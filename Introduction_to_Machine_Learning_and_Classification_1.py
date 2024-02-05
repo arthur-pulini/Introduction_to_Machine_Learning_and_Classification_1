b1774c1dab05fc28b525fe101439e5c8822da89d
Arthur Pulini <arthurpulini@gmail.com>
2024-02-05 12:32:35 -0300

    Name standardization

--- Introduction_to_Machine_Learning_and_Classification_1.py
+++ Introduction_to_Machine_Learning_and_Classification_1.py
@@ -16,20 +16,20 @@ dog2 = [1, 0, 1]
 dog3 = [1, 1, 1]
 
 # 1 => pig , 0 => dog
-datas = [pig1, pig2, pig3, dog1, dog2, dog3]
-classes = [1,1,1,0,0,0]
+trainX = [pig1, pig2, pig3, dog1, dog2, dog3]
+trainY = [1,1,1,0,0,0]
 
-model.fit(datas, classes)
+model.fit(trainX, trainY)
 
 mysterious1 = [1,1,1]
 mysterious2 = [1,1,0]
 mysterious3 = [0,1,1]
 
-tests = [mysterious1, mysterious2, mysterious3]
-predictions = model.predict(tests)
+testsX = [mysterious1, mysterious2, mysterious3]
+testsY = [0,1,1]
+
+predictions = model.predict(testsX)
 print(predictions)
 
-testsClasses = [0,1,1]
-
-accuracyScore = accuracy_score(testsClasses, predictions)
-print("The accuracy was: ", accuracyScore * 100)
+accuracyScore = accuracy_score(testsY, predictions)
+print("The accuracy was: %.2f " % (accuracyScore * 100))
